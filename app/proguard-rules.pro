80360e5e8b18a489ce423bd729eb2d588d83748b
msdumin <msdumin@gmail.com>
2020-10-10 23:05:02 +0800

    secondcommit

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -20,21 +20,3 @@
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
 
--dontwarn okio.**
--keep class okio.** { *;}
-​
--dontwarn okhttp3.**
--keep class okhttp3.** { *;}
-​
--dontwarn com.google.gson.**
--keep class com.google.gson.** { *;}
-​
--dontwarn android.support.**
--keep class android.support.** { *;}
-​
--dontwarn com.ximalaya.ting.android.player.**
--keep class com.ximalaya.ting.android.player.** { *;}
-​
--dontwarn com.ximalaya.ting.android.opensdk.**
--keep interface com.ximalaya.ting.android.opensdk.** {*;}
--keep class com.ximalaya.ting.android.opensdk.** { *; }
--- app/src/main/java/com/example/ximalayaapp/MainActivity.java
+++ app/src/main/java/com/example/ximalayaapp/MainActivity.java
@@ -3,14 +3,44 @@ package com.example.ximalayaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        Map<String, String> map = new HashMap<>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null) {
+                    int size = categories.size();
+                    Log.d(TAG, "onSuccess: categories size = " + size);
+                    for (Category category : categories) {
+                        Log.d(TAG, "onSuccess: category.name = " + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG, "onError: code= " + i + "errorMsg= " + s);
+            }
+        });
     }
 }
